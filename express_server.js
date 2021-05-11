2c7b1a97014776f0f984f786a002e5b6a4594198
Avivit Weissman <wavivit@gmail.com>
2021-05-11 02:06:04 +0000

    add url handler /u to redirect to the long url

--- express_server.js
+++ express_server.js
@@ -51,7 +51,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 
 app.get("/hello", (req, res) => {
