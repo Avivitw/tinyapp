e5d08a23d3f9a46b16c8254b2d2ca468e8a35ecf
Avivit Weissman <wavivit@gmail.com>
2021-05-11 00:34:32 +0000

    remove the require of boddyparse as it canceled... and use express insted

--- express_server.js
+++ express_server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");//set ejs as the view engine
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended: true}));
+const generateRandomString = function() {
+
+};
+
+
+
+app.use(express.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
