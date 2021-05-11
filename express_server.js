fd7dffcb84fd51db98a22db31abf16e59b1f9831
Avivit Weissman <wavivit@gmail.com>
2021-05-11 00:23:18 +0000

    update the body-parser to a higer position

--- express_server.js
+++ express_server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");//set ejs as the view engine
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
