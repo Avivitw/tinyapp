1ec253b4c72c87b43ac4e303cd2eeafcd88d4d21
Avivit Weissman <wavivit@gmail.com>
2021-05-11 01:47:02 +0000

    add a redirect and fixed the /urls/:shortURL

--- express_server.js
+++ express_server.js
@@ -35,18 +35,19 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  let shortURL = generateRandomString();
+   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  console.log(`urlDB`, urlDatabase);
+  // console.log(`urlDB`, urlDatabase);
+  res.redirect(`/urls/${shortURL}`)
+
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
